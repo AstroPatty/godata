6964736fc5a9a475d7a5f129fdaac99f2a83dcfc
Patrick Wells <pwells@ucdavis.edu>
2023-12-07 22:11:53 -0800

    Server updates
    - list_projects now returns error code when collection does not exist
    - list_collections and list_projects now accept "hidden" argument

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,14 +1,20 @@
 use crate::project::ProjectManager;
 use crate::project::get_collection_names;
 use std::sync::{Arc, Mutex};
+use std::convert::Infallible;
+use warp::http::StatusCode;
 
-pub(crate) fn list_collections() -> Vec<String> {
-    let collections = get_collection_names(false);
-    collections.unwrap()
+pub(crate) fn list_collections(show_hidden: bool) -> Result<impl warp::Reply, Infallible> {
+    let collections = get_collection_names(show_hidden);
+    Ok(warp::reply::json(&collections.unwrap()))
 }
 
-pub(crate) fn list_projects(project_manager: Arc<Mutex<ProjectManager>>, collection: String) -> Vec<String> {
-    let projects = project_manager.lock().unwrap().get_project_names(collection, false);
-    println!("projects: {:?}", projects);
-    projects.unwrap()
+pub(crate) fn list_projects(project_manager: Arc<Mutex<ProjectManager>>, collection: String, show_hidden: bool) -> Result<impl warp::Reply, Infallible> {
+    let projects = project_manager.lock().unwrap().get_project_names(collection.clone(), show_hidden);
+    match projects {
+        Ok(project_list) => Ok(warp::reply::with_status(warp::reply::json(&project_list), StatusCode::OK)),
+        Err(_) => Ok(warp::reply::with_status(warp::reply::json(
+            &format!("No collection named {collection}")
+        ), StatusCode::NOT_FOUND))
+    }
 }
--- src/routes.rs
+++ src/routes.rs
@@ -2,6 +2,7 @@ use warp::Filter;
 use crate::{handlers, project};
 use crate::project::ProjectManager;
 use std::sync::{Arc, Mutex};
+use std::collections::HashMap;
 
 
 pub(crate) fn routes(project_manager: Arc<Mutex<ProjectManager>>) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
@@ -13,13 +14,22 @@ pub(crate) fn routes(project_manager: Arc<Mutex<ProjectManager>>) -> impl Filter
 fn list_collections() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     warp::path!("collections")
         .and(warp::get())
-        .map(|| warp::reply::json(&handlers::list_collections()))
+        .and(warp::query::<HashMap<String, bool>>())
+        .map(move |p: HashMap<String, bool>| match p.get("show_hidden") {
+            Some(show_hidden) => handlers::list_collections(*show_hidden),
+            None => handlers::list_collections(false)
+        })
     }
 
 fn list_projects(project_manager: Arc<Mutex<ProjectManager>>) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     warp::path!("projects" / String)
         .and(warp::get())
-        .map(move |collection| warp::reply::json(&handlers::list_projects(project_manager.clone(), collection)))
+        .and(warp::query::<HashMap<String, bool>>())
+        .map(move |collection, p: HashMap<String, bool>| match p.get("show_hidden") {
+            Some(show_hidden) => handlers::list_projects(project_manager.clone(), collection, *show_hidden),
+            None => handlers::list_projects(project_manager.clone(), collection, false)
+        })
+        
 }
 
 fn list_project() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
