0f2110b5eaf21d2c95dd71a51b94f92bcf4dc284
Patrick Wells <patrick.wells.95@gmail.com>
2024-01-31 13:31:59 -0800

    Bump to 0.9.0

--- godata/__init__.py
+++ godata/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "0.8.3"
-__minimum_server_version__ = "0.8.0"
+__version__ = "0.9.0"
+__minimum_server_version__ = "0.9.0"
 
 from .ie import export_project, import_project
 from .project import (
